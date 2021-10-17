0b45815f53d0848dc85abd570263759f2a565069
itsanti <kurov.aleksandr+github@gmail.com>
2021-10-17 13:01:49 +0300

    step 4 done

---
+++ README.md
@@ -0,0 +1,23 @@
+# Watching Storage
+
+Project show first steps in Django ORM. 
+
+# How to start
+
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+### Run
+
+Launch on Linux(Python 3.5) or Windows as simple
+
+```bash
+$ python3 main.py
+```
+
+# Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
--- main.py
+++ main.py
@@ -10,3 +10,20 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+
+    passcards = Passcard.objects.all()
+    print(passcards)
+
+    person42: Passcard = passcards[41]
+    person_info = '''owner_name: {0}
+passcode: {1}
+created_at: {2}
+is_active: {3}'''
+    print(person_info.format(person42.owner_name, person42.passcode, person42.created_at, person42.is_active))
+
+    active_passcards = []
+    for passcard in passcards:
+        if passcard.is_active:
+            active_passcards.append(passcard)
+    print(f'Всего пропусков {len(passcards)}')
+    print(f'Активных пропусков {len(active_passcards)}')
