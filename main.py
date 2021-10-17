fc57af5fa3a827d9b689fc6e1b9240d1df806175
itsanti <kurov.aleksandr+github@gmail.com>
2021-10-17 13:12:16 +0300

    step 6 init

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import os
 
-import django
+from django.core.management import execute_from_command_line
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-django.setup()
-
-from datacenter.models import Passcard  # noqa: E402
-
-if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-
-    passcards = Passcard.objects.all()
-    print(passcards)
-
-    person42: Passcard = passcards[41]
-    person_info = '''owner_name: {0}
-passcode: {1}
-created_at: {2}
-is_active: {3}'''
-    print(person_info.format(person42.owner_name, person42.passcode, person42.created_at, person42.is_active))
-
-    active_passcards = Passcard.objects.filter(is_active=True)
-    print(f'Всего пропусков {len(passcards)}')
-    print(f'Активных пропусков {len(active_passcards)}')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
